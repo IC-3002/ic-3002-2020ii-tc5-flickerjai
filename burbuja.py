b29e45ccc0cc20ae91af4b23c935871ccb77907f
Jailine Gonzalez <jaig1500@gmail.com>
2020-10-03 00:04:22 -0600

    Ejercicios Resueltos

--- burbuja.py
+++ burbuja.py
@@ -1,3 +1,4 @@
+from random import shuffle
 def burbuja(A):
     n = len(A)
     for i in range(1, n):
@@ -11,4 +12,12 @@ def swap(A, i, j):
     A[j] = tmp
 
 def burbuja_optimizado(A):
-    raise NotImplementedError()
+	n = len(A)
+	for i in range(1, n):
+		swapped = False
+		for j in range(0, n - i):
+			if A[j] > A[j + 1]:
+				swap(A, j, j + 1)
+				swapped = True
+		if swapped == False:
+			break 
